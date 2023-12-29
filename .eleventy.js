a6cbfa1f59f1e50df6aa02277f82ee65a5026def
Joel George V <joel.gv3@gmail.com>
2023-12-29 16:36:11 +0000

    Reference folders in eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('CNAME')
-  eleventyConfig.addPassthroughCopy('fonts/arizonia-v21-latin-regular.woff2')
-  eleventyConfig.addPassthroughCopy('assets/IMG_0376.jpg')
+  eleventyConfig.addPassthroughCopy('fonts')
+  eleventyConfig.addPassthroughCopy('assets')
 }
